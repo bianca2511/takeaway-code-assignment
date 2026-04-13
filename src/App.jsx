e580d748b15d9963478da887a3c33efa2703492c
Bianca Eni <b.b.eni@student.tue.nl>
2026-04-13 17:38:49 +0200

    Add logic for querying API based on postcode Fix CORS issue by using vite proxy

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,50 @@
+import { useState } from 'react'
 import './App.css'
 import RestaurantList from './RestaurantList'
 import SearchBar from './SearchBar'
 
 function App() {
+  // state for storing the restaurant list that is fetched
+  const [restaurantList, setRestaurantList] = useState([]);
 
-  return (
-    <>
-    <div className='main-frame'>
-      <h3>Restaurant Finder</h3>
-      <SearchBar></SearchBar>
-      <RestaurantList></RestaurantList>
-    </div>
-    </>
-  )
+  // handle querying the API when the search button is pressed
+  const handleSearch = async (postcode) => {
+    console.log("Searching for:", postcode);
+    const APIURL = `/just-eat/discovery/uk/restaurants/enriched/bypostcode/${encodeURIComponent(postcode)}`;
+
+    try {
+      const response = await fetch(APIURL); //fetch 
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+
+      const result = await response.json(); // get the query result data
+      const rawSelectedRestaurants = result.slice(0, 10); // select only the first 10 restaurants
+      const selectedRestaurants = rawSelectedRestaurants.map((item) => {
+        return {
+          uniqueName: item.uniqueName,
+          name: item.name,
+          rating: item.rating.starRating,
+          address: item.address.firstLine,
+          cuisines: item.cuisisines.map((cuisine) => cuisine.name)
+        }
+      });
+
+      console.log("The first 10 restaurants are: ");
+      console.log(selectedRestaurants);
+      setRestaurantList(selectedRestaurants);
+    } catch (error) {
+      console.log(error.message);
+    }
+    return (
+      <>
+        <div className='main-frame'>
+          <h3>Restaurant Finder</h3>
+          <SearchBar></SearchBar>
+          <RestaurantList></RestaurantList>
+        </div>
+      </>
+    )
+  }
 }
-
-export default App
+  export default App
